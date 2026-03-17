931503a23e9a7d1e9c4e707aa6851a7ea58e18d7
DeepthiSrujana <deepthisrujana.cs24@bmsce.ac.in>
2026-03-17 18:14:19 +0530

    Update sjf_p.c

--- fcfs and sjf/sjf_p.c
+++ fcfs and sjf/sjf_p.c
@@ -35,8 +35,16 @@ int main() {
             }
         }
     }
+     printf("\nPID\tAT\tBT\tCT\tTAT\tWT\n");
+    float total_tat = 0, total_wt = 0;
+printf("1bf24cs338 deepthi  \n ");
     for(i = 0; i < n; i++) {
-        printf("P%d\t%d\t%d\t%d\t%d\t%d\n", i+1, at[i], bt[i], ct[i], tat[i], wt[i]);
+        printf("%d\t%d\t%d\t%d\t%d\t%d\n",
+               pid[i], at[i], bt[i], ct[i], tat[i], wt[i]);
+        total_tat += tat[i];
+        total_wt += wt[i];
     }
+    printf("\nAverage Turnaround Time = %.2f", total_tat / n);
+    printf("\nAverage Waiting Time = %.2f\n", total_wt / n);
     return 0;
 }
